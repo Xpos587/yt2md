240e4c946c96b0390e57942feca18569b4e036a5
Michael <x30827pos@gmail.com>
2024-11-23 00:05:54 +0300

    Fixed a issue with the version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="yt2md",
-    version="0.0.3",
+    version="0.0.4",
     description="Convert YouTube videos to Markdown format",
     long_description=long_description,
     long_description_content_type="text/markdown",
