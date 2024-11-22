feaa78639f866f8088e94f1b775a7db8570e746c
Michael <x30827pos@gmail.com>
2024-11-22 23:53:31 +0300

    retry init

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="yt2md",
-    version="1.0.0",
+    version="0.0.3",
     description="Convert YouTube videos to Markdown format",
     long_description=long_description,
     long_description_content_type="text/markdown",
